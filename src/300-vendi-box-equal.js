e45cea672ef2e884548f00f2b04c488bcc054411
cjhaas <chris@vendiadvertising.com>
2018-09-25 10:11:11 -0500

    Bump to v4

--- src/300-vendi-box-equal.js
+++ src/300-vendi-box-equal.js
@@ -8,7 +8,7 @@
  * more than one set of groups then each group should use their own unique group
  * identifier (similar to radio button groups):
  *
- * <div data-box-equal="group-1">Hello</div>
+ * <div data-box-equal="group-1" data-box-equal-width-disable="">Hello</div>
  * <div data-box-equal="group-1">World</div>
  * <div data-box-equal="group-2">Different</div>
  * <div data-box-equal="group-2">Set</div>
@@ -16,12 +16,15 @@
  * Additionally you may set a global function variable called windows.custom_box_equal_selector
  * that returns an array of items with the above rules in place.
  *
- * @version  3.0.0
+ * @version  4.0.0
  *
  * @history  1.0.0 - Initial version, required manual invocation.
  *           2.0.0 - Added automatic invocation looking for a custom CSS class and custom data-* attribute.
  *           3.0.0 - Removed custom CSS class, now just searching on custom data-* attribute.
  *                 - Added global override function
+ *           4.0.0 - Removed IE7 support
+ *                 - Removed Bob's crazy three spaces
+ *                 - Added ability to turn off at certain widths
  *
  */
 
@@ -31,123 +34,44 @@
 
     var
 
-        /*!
-         * contentloaded.js
-         *
-         * Author: Diego Perini (diego.perini at gmail.com)
-         * Summary: cross-browser wrapper for DOMContentLoaded
-         * Updated: 20101020
-         * License: MIT
-         * Version: 1.2
-         *
-         * URL:
-         * http://javascript.nwbox.com/ContentLoaded/
-         * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
-         *
-         * //From: https://github.com/dperini/ContentLoaded/blob/master/src/contentloaded.js
-         *
-         */
-        createOnReadyEvent = function( fn, win  )
+        get_boxes = function()
         {
-            win = win || window;
-
-            var
-                done = false,
-                top = true,
-
-                doc = win.document,
-                root = doc.documentElement,
-                modern = doc.addEventListener,
-
-                add = modern ? 'addEventListener' : 'attachEvent',
-                rem = modern ? 'removeEventListener' : 'detachEvent',
-                pre = modern ? '' : 'on',
-
-                init = function( e )
-                {
-                    if ( e.type == 'readystatechange' && doc.readyState != 'complete' )
-                    {
-                        return;
-                    }
-
-                    (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
-
-                    if (!done && (done = true)) fn.call(win, e.type || e);
-                },
-
-                poll = function()
-                {
-                    try
-                    {
-                        root.doScroll( 'left' );
-                    }
-                    catch( e )
-                    {
-                        setTimeout( poll, 50 );
-                        return;
-                    }
-
-                    init( 'poll' );
-                }
-            ;
-
-            if ( doc.readyState == 'complete' )
-            {
-                fn.call( win, 'lazy' );
+            //If there is a function attached to the window object called custom_box_equal_selector
+            //then that function will be invoked instead of the regular QSA version.
+            //TODO: We should probably merge the two instead.
+            if(window && window.custom_box_equal_selector && 'function' === typeof window.custom_box_equal_selector){
+                return window.custom_box_equal_selector();
             }
-            else
-            {
-                if ( ! modern && root.doScroll )
-                {
-                    try
-                    {
-                        top = ! win.frameElement;
-                    }
-                    catch( e )
-                    {
-                        //NOOP
-                    }
 
-                    if (top)
-                    {
-                        poll();
-                    }
-                }
+            return document.querySelectorAll( '[data-box-equal]' );
+        },
 
-                doc[add](pre + 'DOMContentLoaded', init, false);
-                doc[add](pre + 'readystatechange', init, false);
-                win[add](pre + 'load', init, false);
-            }
+        get_viewport_width = function()
+        {
+            return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         },
 
         /**
-         * Cross browser event handler.
-         */
-        addEvent = window.addEventListener ?
-                    function (elem, type, method)
-                    {
-                        elem.addEventListener(type, method, false);
-                    }
-                    :
-                    function (elem, type, method)
-                    {
-                        elem.attachEvent('on' + type, method);
-                    },
-
-        /**
-         * Equalize the heights of the items of the given selector.
-         * @param  {string} selector A standard JavaScript/jQuery selector.
+         * Equalize the heights of the given items.
          */
         equalizeHeights = function( items )
         {
             var
                 maxHeight = 0,
+                disable_width = false,
                 i
             ;
 
+            for( i = 0; i < items.length; i++ ){
+                if(items[i].hasAttribute('data-box-equal-width-disable')){
+                    disable_width = parseInt(items[i].getAttribute('data-box-equal-width-disable'));
+                    break;
+                }
+            }
+
             //Calculate the maximum height
-            for( i = 0; i < items.length; i++ )
-            {
+            for( i = 0; i < items.length; i++ ){
+
                 items[i].style.zoom = 1;
                 //Reset the height to auto so that we can handle the resize logic
                 items[i].style.height = 'auto';
@@ -155,75 +79,16 @@
                 maxHeight = Math.max( maxHeight, items[i].clientHeight );
             }
 
+            if(false !== disable_width){
+                if(get_viewport_width() <= disable_width){
+                    return;
+                }
+            }
+
             //Apply the maximum height
-            for( i = 0; i < items.length; i++ )
-            {
+            for( i = 0; i < items.length; i++ ){
                 items[i].style.height = maxHeight + 'px';
             }
-        },
-
-        /**
-         * Create a document.querySelectorAll for IE7
-         */
-        createLegacyQSA = function()
-        {
-            /**
-             * define document.querySelector & document.querySelectorAll for IE7
-             *
-             * A not very fast but small hack. The approach is taken from
-             * http://weblogs.asp.net/bleroy/archive/2009/08/31/queryselectorall-on-old-ie-versions-something-that-doesn-t-work.aspx
-             *
-             */
-            (
-                function ()
-                {
-                    var
-                        style = document.createStyleSheet(),
-                        select =    function (selector, maxCount)
-                                    {
-                                        var
-                                            all = document.all,
-                                            l = all.length,
-                                            i,
-                                            resultSet = []
-                                        ;
-
-                                        style.addRule(selector, "foo:bar");
-                                        for (i = 0; i < l; i += 1)
-                                        {
-                                            if (all[i].currentStyle.foo === "bar")
-                                            {
-                                                resultSet.push(all[i]);
-                                                if (resultSet.length > maxCount)
-                                                {
-                                                    break;
-                                                }
-                                            }
-                                        }
-                                        style.removeRule(0);
-                                        return resultSet;
-
-                                    }
-                    ;
-
-                    //  be rly sure not to destroy a thing!
-                    if (document.querySelectorAll || document.querySelector)
-                    {
-                        return;
-                    }
-
-                    document.querySelectorAll = function (selector)
-                                                {
-                                                    return select(selector, Infinity);
-                                                };
-
-                    document.querySelector =    function (selector)
-                                                {
-                                                    return select(selector, 1)[0] || null;
-                                                };
-                }
-            ()
-            );
         },
 
         /**
@@ -233,7 +98,7 @@
         {
             var
                 //Grab each element with this specific class
-                boxes = ( window && window.custom_box_equal_selector ? window.custom_box_equal_selector() : document.querySelectorAll( '[data-box-equal]' ) ),
+                boxes = get_boxes(),
                 items = [],
                 i, j,
                 id,
@@ -241,89 +106,92 @@
             ;
 
             //Loop through each element found
-            for( i = 0; i < boxes.length; i++ )
-            {
+            for( i = 0; i < boxes.length; i++ ){
 
-            //We're going to set this variable to either an existing
-            //object or we're create a new one. Null it out here
-            //each time to start with
-            obj = null;
+                //We're going to set this variable to either an existing
+                //object or we're create a new one. Null it out here
+                //each time to start with
+                obj = null;
 
-            //Grab our specific data attribute which acts the name attribute
-            //of a radio button.
-            id = boxes[ i ].getAttribute( 'data-box-equal' );
+                //Grab our specific data attribute which acts the name attribute
+                //of a radio button.
+                id = boxes[ i ].getAttribute( 'data-box-equal' );
 
-            //Make sure it is set
-            if( null === id || '' === id )
-            {
-                continue;
+                //Make sure it is set
+                if( null === id || '' === id ){
+                    continue;
+                }
+
+                //Loop through each previously created custom object
+                for( j = 0; j < items.length; j++){
+
+                    //If we found it set our function level variable and
+                    //break out of the inner loop
+                    if( id === items[ j ].id ){
+                        obj = items[ j ];
+                        break;
+                    }
+                }
+
+                //We didn't find our custom local object, create a new one
+                if( null === obj ){
+
+                    obj = {};
+                    //Id will be the contents of the data-box-equal HTML attribute
+                    obj.id = id;
+
+                    //Nodes is an array of each element
+                    obj.nodes = [];
+
+                    //Push onto our collection
+                    items.push( obj );
+                }
+
+                //Obj should now always be set. Push this specific
+                //node onto the collection of nodes for this specific "id"
+                obj.nodes.push( boxes[ i ] );
             }
 
-            //Loop through each previously created custom object
-            for( j = 0; j < items.length; j++)
-            {
-               //If we found it set our function level variable and
-               //break out of the inner loop
-               if( id === items[ j ].id )
-               {
-                  obj = items[ j ];
-                  break;
-               }
+            //The above is basically just a giant sorter and combiner.
+
+            //Loop through each item and set the contents of each item's nodes to
+            //be the same height.
+            for( i = 0; i < items.length; i++ ){
+                equalizeHeights( items[ i ].nodes );
+            }
+        },
+
+       /**
+        * Called immediately after this JS block is loaded.
+        *
+        * NOTE: If this script is loaded in <head> the DOM might not be ready so
+        * this should always delay until DOMContentLoaded
+        *
+        * @param  {string|array} params Either a single selector or an array of selectors.
+        */
+        init = function( )
+        {
+            //We don't support IE7 or less
+            if( ! document.querySelectorAll ){
+                return;
             }
 
-            //We didn't find our custom local object, create a new one
-            if( null === obj )
-            {
-               obj = {};
-               //Id will be the contents of the data-box-equal HTML attribute
-               obj.id = id;
+            //Always handle resize
+            window.addEventListener('resize', onload);
 
-               //Nodes is an array of each element
-               obj.nodes = [];
-
-               //Push onto our collection
-               items.push( obj );
+            if(document.readyState && ('complete' === document.readyState || 'loaded' === document.readyState)){
+                //If the document is already loaded then manually fire the load event
+                onload();
+            }else{
+                //Otherswise delay it until we are loaded
+                document.addEventListener('DOMContentLoaded', onload);
             }
 
-            //Obj should now always be set. Push this specific
-            //node onto the collection of nodes for this specific "id"
-            obj.nodes.push( boxes[ i ] );
-
-         }
-
-         //The above is basically just a giant sorter and combiner.
-
-         //Loop through each item and set the contents of each item's nodes to
-         //be the same height.
-         for( i = 0; i < items.length; i++ )
-         {
-            equalizeHeights( items[ i ].nodes );
-         }
-      },
-
-      /**
-       * Called immediately after this JS block is loaded.
-       *
-       * NOTE: The DOM might not be ready, this should always use addEvent.
-       *
-       * @param  {string|array} params Either a single selector or an array of selectors.
-       */
-      init = function( )
-      {
-         //We require this, won't work in IE7 or less
-         if( ! document.querySelectorAll )
-         {
-            createLegacyQSA();
-         }
-
-         //Bind a load and resize event to actually process our items. Heights aren't always 100% correct until after load
-         //createOnReadyEvent( onload );
-         addEvent( window, 'load',   onload );
-         addEvent( window, 'resize', onload );
-         addEvent( window, 'click', onload );
+            //There was a click handler here previously, not sure why?
+            // window.addEventListener('click', onload);
       }
-   ;
+    ;
 
-   //Kick everything off
-   init( );
+    //Kick everything off
+    init( );
 }());
